205a193a431c3e721dd75c44b70fa8a14ad94ab6
harsh3452 <harshpatil3452@gmail.com>
2026-08-30 19:30:51 +0530

    recursion based code and we are backtracking the carry instead of reversing

--- LinkedList/addOne.java
+++ LinkedList/addOne.java
@@ -26,3 +26,50 @@ class Solution {
         return newHead;
     }
 }
+
+/*Definition of singly linked list:
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+ */
+
+class Solution {
+    public ListNode addOne(ListNode head) {
+        int carry = helper(head.next);
+        if(carry==1){
+            ListNode newHead = new ListNode(1,head);
+            return newHead;
+        }
+        return head;
+    }
+    public int helper(ListNode temp){
+        if(temp == null) return 1;
+        int carry = helper(temp.next);
+        temp.val = temp.val + carry;
+        if(temp.val < 10)
+        {
+            return 0;
+        } 
+        else {
+            temp.val = 0;
+            return 1;
+        }
+    }
+}
+//recursion based code and we are backtracking the carry instead of reversing.
